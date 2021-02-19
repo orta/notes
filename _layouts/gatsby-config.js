5f7ec43abe50c3df175c9670eb19dd845799ea9d
Orta <git@orta.io>
2021-02-19 08:58:04 +0000

    Test deploy

--- _layouts/gatsby-config.js
+++ _layouts/gatsby-config.js
@@ -1,13 +1,12 @@
 const path = require('path')
 
 module.exports = {
-  // pathPrefix: `/foam-template-gatsby-kb`, // a. If you are using github pages, this should be the name of your repo
-  pathPrefix: `/`, // b. If you are using Netlify/Vercel, your can keep it this way
+  pathPrefix: `/notes`,
   siteMetadata: {
     // some SEO configs using by gatsby-theme-kb
-    title: `Foam`, // Replace it with your site's title
-    author: `Your Name`, // Replace it with your name
-    description: `My personal knowledge base`, // Replace it with your site's description
+    title: `Notes`, // Replace it with your site's title
+    author: `Orta Therox`, // Replace it with your name
+    description: `Notes and things`, // Replace it with your site's description
   },
   plugins: [
     {
