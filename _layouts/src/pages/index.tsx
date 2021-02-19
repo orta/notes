1ea48637c64a9e2e1662f71f5c121554d14638e0
Orta <git@orta.io>
2021-02-19 15:01:46 +0000

    Redirects

--- _layouts/src/pages/index.tsx
+++ _layouts/src/pages/index.tsx
@@ -2,7 +2,8 @@ import React, { useEffect} from "react"
 
 export default () => {
   useEffect(() => {
-    if (document.location) document.location.replace("/home")
+    if (document.location && document.location.href.includes("orta")) document.location.replace("/notes/home")
+    if (document.location && document.location.href.includes("localhost")) document.location.replace("/home")
   })
   return <html></html>
 }
