6a4af7ee63f2dd5cb8150ffee2744645f08e349c
Orta Therox <git@orta.io>
2021-02-21 13:52:07 +0000

    More notes and a rough opengraph img generator

--- _layouts/gatsby-config.js
+++ _layouts/gatsby-config.js
@@ -37,9 +37,47 @@ module.exports = {
             options: {
               theme: "github-light",
               useNodeModules: true,
-              nodeModulesTypesPath: path.join(__dirname, "..", "node_modules")
+              nodeModulesTypesPath: path.join(__dirname, "..", "node_modules"),
             },
           });
+
+          // defaultPluginMdx.options.gatsbyRemarkPlugins.push({
+          //   resolve: "gatsby-remark-opengraph",
+          //   options: {
+          //     background: path.join(__dirname, "src", "assets", "bg.png"),
+          //     // if you create post-specific open graph images, be sure to prefix `./public`
+          //     outputPath: (markdownNode) =>
+          //       path.join(
+          //         "./public",
+          //         path.basename(markdownNode.fileAbsolutePath, ".md")
+          //       ),
+          //     texts: (mdNode) => {
+          //       const md = mdNode.internal.content;
+          //       const title = md.split("\n")[0].replace(/#/g, "").trim();
+          //       const preview = md.split("\n")[2].trim().replace(/\[(.*?)\][\[\(].*?[\]\)]/g, '$1')
+
+          //       return [
+          //         {
+          //           text: title,
+          //           y: 120,
+          //           x: 18,
+          //           maxWidth: 1200 - 18 * 2,
+          //           color: "#ffffff",
+          //           font: require.resolve("./src/assets/JMH Typewriter.ttf"),
+          //         },
+          //         {
+          //           text: preview.slice(0, 300),
+          //           fontSize: 80,
+          //           maxWidth: 1200 - 18 * 2,
+          //           font: require.resolve("./src/assets/Hai California.ttf"),
+          //           y: 200,
+          //           x: 18,
+          //           color: "#ffffff",
+          //         },
+          //       ];
+          //     },
+          //   },
+          // });
 
           return defaultPluginMdx;
         },
---
+++ elementary/things-i-miss.md
@@ -0,0 +1,33 @@
+## Things I Miss in elementary OS
+
+I'm a long-term Mac dev, using elementary OS most of the time. I'm happy with the OS, but here's some flaws 
+
+#### No App vs Window distinction
+
+The elementary OS version of cmd + tab works per window, which isn't too offensive of an idea. The issue comes from it's UI implementation. The way to show you what is coming up next is effectively the dock - so you either look at your desktop and guess the next one, or look down at an arbitrary list of app icons with no context which is coming up next.
+
+There's no way I've found to switch between different windows of the same app, so jumping between versions of VS Code requires either mousing, cmd + tabbing and guessing or right clicking on the icon in the dock.
+
+
+#### No Keyboard Discovery
+
+I still haven't figured out how to make a new tab in the terminal via the keyboard. I'll probably read the source code to find that out, but that's not a great way to learn.
+
+#### Many install paths
+
+I have two copies of Discord and Figma, and I have no idea how to disambiguate (or uninstall) them. Linux has a problem with too many "app sandbox"-y things like Flatpak vs Snap vs AppImage. I don't get the differences yet, and if I have something installed from one of those, and via the store it's hard to know which is the right one.
+
+
+#### No "Desktop"
+
+I don't want a desktop folder per-say but I want a place to dump things easily which has a single keystroke to access from save panels.
+
+
+##### Quicklook
+
+Press spacebar to preview files 
+
+## Apps I want
+
+- An alt-tab replacer
+- Something like [Fresh](https://ironicsoftware.com/fresh/)
