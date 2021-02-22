8cb971c740828e18af12c8df9cc24300bd7ef8ad
Orta Therox <git@orta.io>
2021-02-22 10:32:58 +0000

    Get og images built

--- _layouts/gatsby-config.js
+++ _layouts/gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     description: `Notes and things`, // Replace it with your site's description
   },
   plugins: [
+    'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-theme-kb`,
       options: {
@@ -41,59 +42,48 @@ module.exports = {
             },
           });
 
-          // defaultPluginMdx.options.gatsbyRemarkPlugins.push({
-          //   resolve: "gatsby-remark-opengraph",
-          //   options: {
-          //     background: path.join(__dirname, "src", "assets", "bg.png"),
-          //     // if you create post-specific open graph images, be sure to prefix `./public`
-          //     outputPath: (markdownNode) =>
-          //       path.join(
-          //         "./public",
-          //         path.basename(markdownNode.fileAbsolutePath, ".md")
-          //       ),
-          //     texts: (mdNode) => {
-          //       const md = mdNode.internal.content;
-          //       const title = md.split("\n")[0].replace(/#/g, "").trim();
-          //       const preview = md.split("\n")[2].trim().replace(/\[(.*?)\][\[\(].*?[\]\)]/g, '$1')
+          defaultPluginMdx.options.gatsbyRemarkPlugins.push({
+            resolve: "gatsby-remark-opengraph",
+            options: {
+              background: path.join(__dirname, "src", "assets", "bg.png"),
+              outputPath: (markdownNode) => {
+                const base = path.join(__dirname, "..")
+                return path.join(
+                  "./public",
+                  markdownNode.fileAbsolutePath.replace(base, "").replace(".md", "")
+                )
+              },
+              texts: (mdNode) => {
+                const md = mdNode.internal.content;
+                const title = md.split("\n")[0].replace(/#/g, "").trim();
+                const preview = md.split("\n")[2].trim().replace(/\[(.*?)\][\[\(].*?[\]\)]/g, '$1')
 
-          //       return [
-          //         {
-          //           text: title,
-          //           y: 120,
-          //           x: 18,
-          //           maxWidth: 1200 - 18 * 2,
-          //           color: "#ffffff",
-          //           font: require.resolve("./src/assets/JMH Typewriter.ttf"),
-          //         },
-          //         {
-          //           text: preview.slice(0, 300),
-          //           fontSize: 80,
-          //           maxWidth: 1200 - 18 * 2,
-          //           font: require.resolve("./src/assets/Hai California.ttf"),
-          //           y: 200,
-          //           x: 18,
-          //           color: "#ffffff",
-          //         },
-          //       ];
-          //     },
-          //   },
-          // });
+                return [
+                  {
+                    text: title,
+                    y: 120,
+                    x: 18,
+                    maxWidth: 1200 - 18 * 2,
+                    color: "#ffffff",
+                    font: require.resolve("./src/assets/Roboto-Bold.ttf"),
+                  },
+                  {
+                    text: preview.slice(0, 300),
+                    fontSize: 30,
+                    maxWidth: 1200 - 18 * 2,
+                    font: require.resolve("./src/assets/Roboto-Regular.ttf"),
+                    y: 200,
+                    x: 18,
+                    color: "#ffffff",
+                  },
+                ];
+              },
+            },
+          });
 
           return defaultPluginMdx;
         },
       },
-    },
-    {
-      // this plugin makes sure your static files will be served by gatsby,
-      //   but of course you need to reference them by absolute path, e.g. '/assets/img.png'.
-      // if you have multiple directories, copy this plugin section and specify other directory
-      // check https://github.com/csath/gatsby-plugin-copy-files-enhanced to find docs for this plugin
-      resolve: "gatsby-plugin-copy-files-enhanced",
-      options: {
-        source: path.resolve(__dirname, `../assets`),
-        destination: "/assets",
-        purge: false,
-      },
-    },
+    }
   ],
 };
--- _layouts/src/gatsby-theme-kb/components/Topic/index.tsx
+++ _layouts/src/gatsby-theme-kb/components/Topic/index.tsx
@@ -2,13 +2,19 @@ import React, { useEffect } from "react"
 import { setupTwoslashHovers } from "shiki-twoslash/dist/dom";
 import Topic from "gatsby-theme-kb/src/components/Topic/index"
 import "../../../app.css"
+import Helmet from "react-helmet"
+
 
 export default function MyTopic(props) {
   useEffect(setupTwoslashHovers, [])
 
   return (
     <div className="orta">
+         <Helmet>
+           <meta property="og:image" content="./og-image.jpg" />
+        </Helmet>
       <Topic {...props} />
     </div>
   )
 }
+ 
